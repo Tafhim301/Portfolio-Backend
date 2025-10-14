93838ff236713b91d0cb905f19b39b47bc02fa74
Tafhim301 <tafhim301@gmail.com>
2025-10-14 11:13:44 +0600

    Added schema for blog and user

---
+++ prisma/schema/migrations/20251014051138_schema_setup/migration.sql
@@ -0,0 +1,91 @@
+/*
+  Warnings:
+
+  - You are about to drop the `admins` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `doctors` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `patients` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."admins" DROP CONSTRAINT "admins_email_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."doctors" DROP CONSTRAINT "doctors_email_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."patients" DROP CONSTRAINT "patients_email_fkey";
+
+-- DropTable
+DROP TABLE "public"."admins";
+
+-- DropTable
+DROP TABLE "public"."doctors";
+
+-- DropTable
+DROP TABLE "public"."patients";
+
+-- DropTable
+DROP TABLE "public"."users";
+
+-- DropEnum
+DROP TYPE "public"."Gender";
+
+-- DropEnum
+DROP TYPE "public"."UserRole";
+
+-- DropEnum
+DROP TYPE "public"."UserStatus";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'OWNER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "excerpt" TEXT NOT NULL,
+    "coverImage" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "thumbnail" TEXT NOT NULL,
+    "liveUrl" TEXT NOT NULL,
+    "repoUrl" TEXT NOT NULL,
+    "features" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Blog_slug_key" ON "Blog"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
